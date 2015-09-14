27961f8f39abce8a56442c0331138d585c4541a0
hxdone <henryadam@sohu.com>
2015-09-14 18:11:10 +0800

    add python module usage demo

---
+++ python/module_test/callee.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# in python, each .py file is a module.
+
+greeting="hello, world!"
---
+++ python/module_test/caller.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# illustrate how to import a module
+
+import callee
+
+print callee.greeting
+print __name__
+print callee.__name__
